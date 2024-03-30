9240cb66e0c89d5793fa76f8fb3b97650e5af0a0
Yut Fut <seregey.golubev@mail.ru>
2024-03-31 02:29:17 +0300

    added switch

--- cmd/main.go
+++ cmd/main.go
@@ -6,16 +6,18 @@ import (
 	"log"
 
 	"redis/internal/redisRepository"
-	// "redis/internal/memcachedRepository"
+	"redis/internal/memcachedRepository"
 
 	"redis/pkg/conf"
 	"redis/pkg/redis"
-	// "redis/pkg/memcached"
+	"redis/pkg/memcached"
 
 	auth "redis/internal/auth"
 
 	"github.com/gofiber/fiber/v3"
 )
+
+const db = "redis"
 
 func main() {
 	flagPath := flag.String("conf_path", "./pkg/conf/conf.json", "path to config")
@@ -25,22 +27,28 @@ func main() {
 		log.Fatal(err)
 	}
 
-	redisConn, err := redis.RedisConn(config)
-	if err != nil {
-		log.Fatal(err)
+	var AuthHandler auth.AuthHandlerInterface
+
+	switch db{
+	case "redis":
+		redisConn, err := redis.RedisConn(config)
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+		redisDB := redisrepository.NewRedisRepository(redisConn)
+		AuthHandler = auth.NewAuthHandler(redisDB)
+	case "memcached":
+		memcachedConn, err := memcached.MemcachedConn(config)
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+		memcachedDB := memcachedRepository.NewMemcachedRepository(memcachedConn)
+		AuthHandler = auth.NewAuthHandler(memcachedDB)
+	default:
+		log.Fatal(db)
 	}
-
-	redisDB := redisrepository.NewRedisRepository(redisConn)
-	AuthHandler := auth.NewAuthHandler(redisDB)
-
-	// memcachedConn, err := memcached.MemcachedConn(config)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// memcachedDB := memcachedRepository.NewMemcachedRepository(memcachedConn)
-	// AuthHandler := auth.NewAuthHandler(memcachedDB)
-
 
 	router := fiber.New()
 	auth.NewAuthRouting(router, AuthHandler)
